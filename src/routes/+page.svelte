6c79f5cf622da52dc659c80cf1034cfe17bfddcf
morhi-2214 <kamejump2214@gmail.com>
2025-01-03 17:01:24 +0900

    feat: 一覧ページを作成

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import type { PageData } from './$types';
+	export let data: PageData;
+</script>
+
+<svelte:head>
+	<title>Newt・SvelteKitブログ</title>
+	<meta name="description" content="NewtとSvelteKitを利用したブログです" />
+</svelte:head>
+
+<main>
+	<ul>
+		{#each data.articles as article (article._id)}
+			<li>
+				<a href={`articles/${article.slug}`}>{article.title}</a>
+			</li>
+		{/each}
+	</ul>
+</main>
