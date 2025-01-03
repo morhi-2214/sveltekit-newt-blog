1b424da2478c0d82c68dcaf91182f213d03de780
morhi-2214 <kamejump2214@gmail.com>
2025-01-03 17:01:38 +0900

    feat: 詳細ページを作成

---
+++ src/routes/articles/[slug]/+page.server.ts
@@ -0,0 +1,18 @@
+import { newtClient } from '$lib/server/newt';
+import type { Article } from '$lib/server/newt';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ params }) => {
+	const article = await newtClient.getFirstContent<Article>({
+		appUid: 'blog',
+		modelUid: 'article',
+		query: {
+			slug: params.slug,
+			select: ['_id', 'title', 'slug', 'body']
+		}
+	});
+
+	return {
+		article
+	};
+};
---
+++ src/routes/articles/[slug]/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	export let data: PageData;
+</script>
+
+<svelte:head>
+	<title>{data.article?.title}</title>
+	<meta name="description" content="投稿詳細ページです" />
+</svelte:head>
+
+<main>
+	<h1>{data.article?.title}</h1>
+	<div>{@html data.article?.body}</div>
+</main>
