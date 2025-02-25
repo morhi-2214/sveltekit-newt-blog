3631ba8f2b83e7400d2aee1741f7c21ad1c853d8
morhi-2214 <kamejump2214@gmail.com>
2025-02-25 16:56:45 +0900

    feat: 型情報を更新

--- src/lib/server/newt.ts
+++ src/lib/server/newt.ts
@@ -6,7 +6,7 @@ export interface Article {
 	_sys: {
 		createdAt: string;
 		updatedAt: string;
-		customOrder: number;
+		customOrder: 3;
 		raw: {
 			createdAt: string;
 			updatedAt: string;
@@ -16,7 +16,83 @@ export interface Article {
 	};
 	title: string;
 	slug: string;
+	meta: {
+		title: string;
+		description: string;
+		ogImage: {
+			_id: string;
+			src: string;
+			fileName: string;
+			fileType: string;
+			fileSize: number;
+			width: number;
+			height: number;
+			title: string;
+			altText: string;
+			description: string;
+		};
+	};
 	body: string;
+	coverImage: {
+		_id: string;
+		src: string;
+		fileName: string;
+		fileType: string;
+		fileSize: number;
+		width: number;
+		height: number;
+		title: string;
+		altText: string;
+		description: string;
+		author: string;
+		tags: string[];
+	};
+}
+
+export interface Author {
+	_id: string;
+	_sys: {
+		createdAt: string;
+		updatedAt: string;
+		customOrder: number;
+		raw: {
+			createdAt: string;
+			updatedAt: string;
+			firstPublishedAt: string;
+			publishedAt: string;
+		};
+	};
+	fullName: string;
+	slug: string;
+	biography: string;
+	profileImage: {
+		_id: string;
+		src: string;
+		fileName: string;
+		fileType: string;
+		fileSize: number;
+		width: number;
+		height: number;
+		title: string;
+		altText: string;
+		description: string;
+	};
+}
+export interface Tag {
+	_id: string;
+	_sys: {
+		createdAt: string;
+		updatedAt: string;
+		customOrder: number;
+		raw: {
+			createdAt: string;
+			updatedAt: string;
+			firstPublishedAt: string;
+			publishedAt: string;
+		};
+	};
+	name: string;
+	slug: string;
 }
 
 export const newtClient = createClient({
